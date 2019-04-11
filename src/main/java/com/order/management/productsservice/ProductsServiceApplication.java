021d3034a9eee21692ddd9935cab23e252ce5899
csaatgit <30920913+csaatgit@users.noreply.github.com>
2019-04-11 22:04:40 +0530

    removing errors

--- src/main/java/com/order/management/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/order/management/productsservice/ProductsServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
